63673ca70601ee8568764c0b5e3fb3d50ffba5a5
Corey Lindsey <70921550+Vierbyte@users.noreply.github.com>
2022-06-07 02:47:27 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   reactStrictMode: true,
   images: {
     domains: ['bayut-production.s3.eu-central-1.amazonaws.com']
